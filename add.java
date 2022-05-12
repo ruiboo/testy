4bcdba9a65dc2d7225b97e9ddb131f6e61bdad32
Jared McKinney <jmckinney@microsoft.com>
2022-05-12 10:57:33 -0400

    created function to add 2 numbers

---
+++ add.java
@@ -0,0 +1,10 @@
+class add{
+
+    public static void main(String[] args) {
+        System.out.println(addNum(1,2));
+    }
+
+    public static int addNum(int a, int b) {
+        return a - b;
+    }
+}
